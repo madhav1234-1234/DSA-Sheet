cfa0dac76a4a1f45f50bd77cdf6c2139f13a5d48
Madhav kumar <73577294+madhav1234-1234@users.noreply.github.com>
2023-01-12 18:07:56 +0530

    Update 2.cpp

--- DAY1/2.cpp
+++ DAY1/2.cpp
@@ -11,21 +11,17 @@ int missing(int arr[],int n){
     return sum1-sum;
 }
 int repeating(int arr[],int n){
-    int sum=(n*(n+1)*(2*n+1))/6;
-    int sum1=0;
-    int sum2=n*(n+1)/2;
-    int sum3=0;
+    int sum1,sum2,sum3=0,sum4=0;
+    sum1=n*(n+1)/2;
+    sum2=(n*(n+1)*(2*n+1))/6;
     for(int i=0;i<n;i++){
-        sum3=sum3+arr[i];
+        sum3+=arr[i];
     }
     for(int i=0;i<n;i++){
-        sum1=sum1+(arr[i]*arr[i]);
+        sum4+=(arr[i]*arr[i]);
     }
-    int res=sum1-sum;
-    int res1=sum3-sum2;
-    int ans=res/res1;
-    int ans1=ans-res1;
-    return ans1;
+    int res=(sum4-sum2)/(sum3-sum1);
+    return res-(sum3-sum1);
 }
 int main(){
     int n;
